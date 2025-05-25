88a5a2b6de9b046a350d1973a3ef73da38d28be0
Mayur Ghoghari <46015401+Mayur913@users.noreply.github.com>
2025-05-25 12:32:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById('toggle');
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+});
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark');
+}
